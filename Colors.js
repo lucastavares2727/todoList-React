432e5c5ff5940c6fcf36e5eb39ae15acd91ed8ed
lucastavares2727 <lucas.a.tavares27@gmail.com>
2024-03-16 12:55:41 -0400

    todoListReact

---
+++ Colors.js
@@ -0,0 +1,9 @@
+export default colors= {
+
+    black: "#2D3436",
+    blue: "#24A6D9",
+    gray:"#A4A4A4",
+    lightGray:"CACACA",
+    lightBlue:"#A7CBD9",
+    white:"#FFFFFF"
+};
---
+++ tempData.js
@@ -0,0 +1,82 @@
+//Dadoos temporais ate conectar com o firebase
+
+
+export default tempData = [
+
+    {
+        id:1,
+        name: "Plan a Trip",
+        color:"#24A6D9",
+        todos: [
+
+            {
+
+                title:"Book Flight",
+                completed: false
+            },
+            {
+                title:"Passaport Check",
+                completed: true
+            },
+            {
+                title:"Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title:"Pack Luggage",
+                completed: false
+            }
+            ]
+    },
+
+
+    {
+    id:2,
+    name :"Errands",
+    color:"#8022D9",
+    todos :[
+
+        {
+            title: "Buy Milk",
+            completed:false
+        },
+        {
+            title:"Planm weekend camping trip",
+            completed:true
+        },
+        {
+            title:"Run 2 miles",
+            completed:true
+        },
+        {
+            title:"Make a Video",
+            completed:true
+        },
+        {
+            title:"Take dog for walk",
+            completed:true
+        }
+    ]
+    },
+    {
+
+    id:3,    
+    name: "BirthDay Party",
+    color:"#595BD9",
+    todos:[
+
+        {
+            title:"Get Ballons",
+            completed:false
+        },
+        {
+            title:"Send Invitations",
+            completed:true
+        },
+        {
+            title:"Make Dinner Reservations",
+            completed:true
+        }
+    ]
+    }
+];
